9bb7523743ccda73fdb0084f5ebd853d647e6de2
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-04-08 20:56:12 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -2,11 +2,38 @@ import * as functions from "./modules/functions.js";
 
 functions.isWebp();
 
-// import Swiper, { Navigation, Pagination } from 'swiper';
+import Swiper, { Navigation, Pagination } from 'swiper';
 
-// const swiper = new Swiper();
+const swiper = new Swiper();
 
 var phoneMask = IMask(
-    document.getElementById('tel'), {
-      mask: '+{7}(000)000-00-00'
-    });
+  document.getElementById('tel'), {
+    mask: '+{7}(000)000-00-00'
+  }
+);
+
+$(document).ready(function() {
+  $(".accordion > .accordion__button").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".accordion__content")
+        .slideUp(200);
+    } else {
+      $(".accordion > .accordion__button").removeClass("active");
+      $(this).addClass("active");
+      $(".accordion__content").slideUp(200);
+      $(this)
+        .siblings(".accordion__content")
+        .slideDown(200);
+    }
+  });
+});
+
+var works = new Swiper(".works__slider", {
+  slidesPerView: "auto",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
